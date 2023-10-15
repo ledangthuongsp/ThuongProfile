38c3f0030e8bd7657baca829ccc8b3f1566959c3
Lê Đăng Thương <21522654@gm.uit.edu.vn>
2023-10-16 00:10:08 +0700

    Xong navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 // App.js
 import React from 'react';
 import Navigation from './navigation/Navigation';
+import { Helmet } from 'react-helmet';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Profile";
+  }, []);
+
   return (
     <div className="App">
       <Navigation />
-      {/* Các nội dung khác của ứng dụng */}
+      <Helmet>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
     </div>
   );
 }
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -4,11 +4,13 @@ import '../navigation/Navigation.css';
 
 function Navigation() {
   return (
+    
     <nav className="navigation">
+      <img src='https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/265706575_439139374425044_4439484882170640413_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=HnQr5FfL_DAAX9wzK0a&_nc_ht=scontent.fsgn5-6.fna&_nc_e2o=f&oh=00_AfAA9mfD33eG9GkJX4rBfh6mhSX8qZxDkvQvlv_6rtwshQ&oe=6530993B' alt='Ảnh đại diện'/>
       <ul>
-        <li><a href="/">Home Page</a></li>
+        <li><a href="/">Work</a></li>
         <li><a href="/about">About Me</a></li>
-        <li><a href="/products">My Product</a></li>
+        <li><a href="/products">Blog</a></li>
         <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
