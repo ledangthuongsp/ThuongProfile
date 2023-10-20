d321d887a11834f33f9ed8add1a3e08313033a1a
Lê Đăng Thương <21522654@gm.uit.edu.vn>
2023-10-21 01:16:43 +0700

    Create font ROBOTO

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navigation from './navigation/Navigation';
 import { Helmet } from 'react-helmet';
 import { useEffect } from 'react';
+import AboutMe from './component/aboutme/aboutme';
 
 function App() {
   useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Helmet>
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/component/aboutme/aboutme.js
@@ -0,0 +1,12 @@
+// AboutMe.js
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <h1>Hello, I'm</h1>
+    </div>
+  );
+}
+
+export default AboutMe;
