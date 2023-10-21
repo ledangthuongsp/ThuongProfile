73a188fed27e77835e7da20631741200a2b2c97c
Lê Đăng Thương <21522654@gm.uit.edu.vn>
2023-10-21 15:15:28 +0700

    Cách filter dự án

--- src/component/project/project.js
+++ src/component/project/project.js
@@ -1,18 +1,41 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import "../project/project.css";
-// Tạo một đối tượng đại diện cho thư mục assets
-function project() {
-  return (
-    <div className="project-container">
-    <ul>
-      <li><a href="/">All</a></li>
-      <li><a href="/about">Mobile</a></li>
-      <li><a href="/products">Website</a></li>
-      <li><a href="/contact">Game</a></li>
-    </ul>
-  </div>
 
+const projects = [
+  { id: 1, name: 'Dự án 1', type: 'Mobile' },
+  { id: 2, name: 'Dự án 2', type: 'Website' },
+  { id: 3, name: 'Dự án 3', type: 'Game' },
+  { id: 5, name: 'Dự án 5', type: 'Mobile' },
+  { id: 6, name: 'Dự án 6', type: 'Website' },
+  { id: 7, name: 'Dự án 7', type: 'Game' },
+  // Thêm các dự án khác ở đây
+];
+
+function Project() {
+  const [filter, setFilter] = useState('All');
+  const filteredProjects = filter === 'All' ? projects : projects.filter(project => project.type === filter);
+
+  return (
+    <div>
+      <div className="project-container">
+        <ul>
+          <li><a href="#" onClick={() => setFilter('All')}>All</a></li>
+          <li><a href="#" onClick={() => setFilter('Mobile')}>Mobile</a></li>
+          <li><a href="#" onClick={() => setFilter('Website')}>Website</a></li>
+          <li><a href="#" onClick={() => setFilter('Game')}>Game</a></li>
+        </ul>
+      </div>
+      <div className="personal-project">
+        <ul>
+          {filteredProjects.map(project => (
+            <li key={project.id}>
+              <a href={`/project/${project.id}`}>{project.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
-export default project;
+
+export default Project;
