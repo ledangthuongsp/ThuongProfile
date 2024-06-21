17c46269c6d182c725e27dcd69ac1439f01b0661
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-06-21 22:50:48 +0700

    Cập nhật logo

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="bg-dark-purple shadow-md p-4">
+        <nav className="bg-[var(--teal-dark-2)] shadow-md p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="text-xl font-bold text-white">Sb.</div>
-                <div className="flex space-x-4 text-white">
+                <div className="text-xl font-bold text-[var(--teal-dark-5)]">Piller</div>
+                <div className="hidden md:flex space-x-4 text-[var(--teal-dark-5)]">
                     <a href="#home">Home</a>
                     <a href="#about">About</a>
                     <a href="#projects">Projects</a>
                     <a href="#resume">Resume</a>
                     <a href="#blogs">Blogs</a>
                 </div>
-                <div className="text-white">⭐</div>
+                <div className="text-[var(--teal-dark-5)]">⭐</div>
             </div>
         </nav>
     );
