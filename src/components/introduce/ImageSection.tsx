0c14e6488fb876d41bf6af2fd330dc10de0e68d6
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-06-22 21:01:58 +0700

    Tạm thời oke introduce

---
+++ src/components/introduce/ImageSection.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ImageSection.css';
+
+const ImageSection: React.FC = () => {
+    return (
+        <section className="image-section">
+            <div className="image-section-content">
+                <button className="image-section-button">
+                    Get in Touch
+                </button>
+            </div>
+        </section>
+    );
+};
+
+export default ImageSection;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { FaInstagram, FaLinkedin, FaFacebook, FaGithub } from 'react-icons/fa';
 import Type from '../../components/type/type';
 import './styles.css';
 import Navbar from '../../components/navbar/Navbar';
 import AvatarShape from '../../components/avatar/Avatar';
+import Introduce from '../../components/introduce/Introduce';
+import ImageSection from '../../components/introduce/ImageSection';
 
 const Home: React.FC = () => {
     return (
         <>
             <Navbar />
-            <section className="home-section home-overlay" id="home">
+            <section className="home-section" id="home">
                 <div className="home-content">
                     <div className="home-header">
                         <h1 className="heading">
@@ -20,10 +23,26 @@ const Home: React.FC = () => {
                         <div className="type-wrapper">
                             <Type />
                         </div>
+                        <div className="social-icons">
+                            <a href="https://www.instagram.com/_ddawng.pil" target="_blank" rel="noopener noreferrer">
+                                <FaInstagram className="icon" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/p1ller" target="_blank" rel="noopener noreferrer">
+                                <FaLinkedin className="icon" />
+                            </a>
+                            <a href="https://www.facebook.com/ldt203.pw/" target="_blank" rel="noopener noreferrer">
+                                <FaFacebook className="icon" />
+                            </a>
+                            <a href="https://github.com/LeDangThuong" target="_blank" rel="noopener noreferrer">
+                                <FaGithub className="icon" />
+                            </a>
+                        </div>
                     </div>
                     <AvatarShape />
                 </div>
             </section>
+            <ImageSection />
+            <Introduce />
         </>
     );
 };
