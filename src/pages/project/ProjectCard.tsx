14e3afe0568837609b1eb71e4c47dcfdbb982447
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-06-24 23:49:50 +0700

    Fixx xong component

--- src/pages/project/ProjectCard.tsx
+++ src/pages/project/ProjectCard.tsx
@@ -13,25 +13,27 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = (props) => {
     return (
-        <div className="border rounded-lg shadow-lg overflow-hidden">
+        <div className="flex flex-col justify-between h-full border rounded-lg shadow-lg overflow-hidden bg-gray-800">
             <img src={props.imgPath} alt="Project" className="w-full h-48 object-cover" />
-            <div className="p-6">
-                <h2 className="text-2xl font-bold mb-2">{props.title}</h2>
-                <p className="text-gray-700 mb-4">{props.description}</p>
-                <a href={props.ghLink} target="_blank" rel="noopener noreferrer">
-                    <button className="flex-1 items-center justify-center bg-blue-500 text-white px-4 py-2 rounded-md mr-2">
-                        <BsGithub className="mr-2" />
-                        {props.isBlog ? "Blog" : "GitHub"}
-                    </button>
-                </a>
-                {props.demoLink && !props.isBlog && (
-                    <a href={props.demoLink} target="_blank" rel="noopener noreferrer">
-                        <button className="flex-1 items-center justify-center bg-blue-500 text-white px-4 py-2 rounded-md ml-2">
-                            <CgWebsite className="mr-2" />
-                            Demo
+            <div className="p-6 flex flex-col flex-grow">
+                <h2 className="text-2xl font-bold mb-2 text-white">{props.title}</h2>
+                <p className="text-gray-300 flex-grow">{props.description}</p>
+                <div className="mt-4">
+                    <a href={props.ghLink} target="_blank" rel="noopener noreferrer">
+                        <button className="flex-1 items-center bg-purple-600 text-white px-4 py-2 rounded-md mr-2">
+                            <BsGithub className="mr-2" />
+                            {props.isBlog ? "Blog" : "GitHub"}
                         </button>
                     </a>
-                )}
+                    {props.demoLink && !props.isBlog && (
+                        <a href={props.demoLink} target="_blank" rel="noopener noreferrer">
+                            <button className="flex-1 items-center bg-purple-600 text-white px-4 py-2 rounded-md ml-2">
+                                <CgWebsite className="mr-2" />
+                                Demo
+                            </button>
+                        </a>
+                    )}
+                </div>
             </div>
         </div>
     );
