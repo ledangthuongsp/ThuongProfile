2f0255f5259fa745de8ee5dcb8a3b483c3e706e5
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-07-08 23:35:43 +0700

    Cập nhật thêm resume vào assets

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-
+import CV from '../../assets/ledangthuong_resume_internship.pdf'
 const Resume: React.FC = () => {
-    return (
-        <>
-            <div className="flex flex-col">
-                <div className="flex ">
-                    <h1>Resume Page</h1>
-                </div>
+    const handleDownload = () => {
+        const link = document.createElement("a");
+        link.href = CV; // Replace with the path to your PDF file
+        link.download = "resume.pdf";
+        link.click();
+    };
 
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="flex flex-col items-center">
+                <h1 className="mb-4">Download Le Dang Thuong Resume</h1>
+                <button
+                    onClick={handleDownload}
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                >
+                    Download Resume
+                </button>
             </div>
-        </>
+        </div>
     );
-}
+};
+
 export default Resume;
